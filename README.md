d10fec635882935035806635c267269a0753f378
Juvenal Yescas <juvenal@mail.com>
2019-07-12 12:37:41 -0500

    update assert 4

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
-> https://github.com/dwyl/learn-circleci
+> https://docs.microsoft.com/en-us/azure/devops/pipelines/languages/python?view=azure-devops
+ 
--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,7 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert 5 == calculator.add(2, 2)
+        assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
